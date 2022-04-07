6c087818065b164b868b4025f3183a2ab246ef6f
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-07 11:18:22 -0300

    exercicios tipos primitivos, if/else e switch case

---
+++ fundamentos-do-desenvolvimento-web/Bloco_04_Introdução_JavaScript_e_Lógica_de_Programação/01_Introdução_JavaScript/scripts3.js
@@ -0,0 +1,18 @@
+const nota = 80
+
+switch (nota) {
+    case nota < 60:
+    console.log("Você foi reprovada(o)");
+    break;
+
+    case nota => 60 && nota < 80:
+    console.log("Você está na nossa lista de espera");
+    break;
+
+    case nota => 80:
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+
+    default:
+     console.log("null");
+}
