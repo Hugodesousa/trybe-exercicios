f9da6d318a406bfc80c38c77d6e92dd9d7064e51
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-07 15:26:04 -0300

    correção

--- fundamentos-do-desenvolvimento-web/Bloco_04_Introdução_JavaScript_e_Lógica_de_Programação/01_Introdução_JavaScript/scripts3.js
+++ fundamentos-do-desenvolvimento-web/Bloco_04_Introdução_JavaScript_e_Lógica_de_Programação/01_Introdução_JavaScript/scripts3.js
@@ -1,18 +1,18 @@
-const nota = 59
+let nota = 59
 
 switch (nota) {
     case nota < 60:
-    console.log("Você foi reprovada(o)");
+    console.log("Você foi reprovada(o)")
     break;
 
     case nota => 60 && nota < 80:
-    console.log("Você está na nossa lista de espera");
+    console.log("Você está na nossa lista de espera")
     break;
 
     case nota => 80:
-    console.log("Parabéns, você foi aprovada(o)!");
+    console.log("Parabéns, você foi aprovada(o)!")
     break;
 
     default:
-     console.log("null");
+     console.log("null")
 }
