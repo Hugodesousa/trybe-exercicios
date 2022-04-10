98bb4544dc00ab69c938edbba424bcd5b5f9de2c
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-10 17:06:59 -0300

    exercicio 8

---
+++ fundamentos-do-desenvolvimento-web/Bloco_04_Introdução_JavaScript_e_Lógica_de_Programação/01_Introdução_JavaScript/ex8.js
@@ -0,0 +1,11 @@
+const number1 = 7;
+const number2 = 12;
+const number3 = 57;
+let test = true;
+
+if ((number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0)){
+    test = true
+} else {
+    test = false
+}
+console.log(test)
