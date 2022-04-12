0ef86e17a8e95891ab0af87490824b9bf4c56101
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-11 21:03:33 -0300

    para ficar correção

--- fundamentos-do-desenvolvimento-web/Bloco_04_Introdução_JavaScript_e_Lógica_de_Programação/04_Objetos_e_funções/test01.js
+++ fundamentos-do-desenvolvimento-web/Bloco_04_Introdução_JavaScript_e_Lógica_de_Programação/04_Objetos_e_funções/test01.js
@@ -16,24 +16,24 @@
 
 //Exemplo 2: notação com pontos e []
 
-// let conta = {
-//     agencia: '0000',
-//     banco: {
-//       cod: '012',
-//       id: 4,
-//       nome: 'TrybeBank',
-//     },
-//   };
+let conta = {
+    agencia: '0000',
+    banco: {
+      cod: '012',
+      id: 4,
+      nome: 'TrybeBank',
+    },
+  };
   
-//   let infoDoBanco = 'banco';
-//   console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
-//   console.log(conta[infoDoBanco]['nome']); // TrybeBank
+  let infoDoBanco = 'banco';
+  console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+  console.log(conta[infoDoBanco]['nome']); // TrybeBank
   
-//   console.log(conta.agencia); // 0000
-//   console.log(conta['agencia']); // 0000
+  console.log(conta.agencia); // 0000
+  console.log(conta['agencia']); // 0000
   
-//   console.log(conta.banco.cod); // 012
-//   console.log(conta['banco']['id']); // 4
+  console.log(conta.banco.cod); // 012
+  console.log(conta['banco']['id']); // 4
 
 // Exemplo 3: chaves dentro de chaves
 
