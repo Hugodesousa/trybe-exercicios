46dab6a0390d5dc097800ebec6dedda458ddf08e
Hugodesousa <hugodesousa.16@gmail.com>
2022-04-13 22:39:43 -0300

    exercicio de querySelector

---
+++ fundamentos-do-desenvolvimento-web/Bloco 05_JavaScript_DOM_Eventos_e_Web_Storage/01_JavaScript _DOM_e_seletores/script.js
@@ -0,0 +1,11 @@
+let emergencyTasks = document.querySelector(".emergency-tasks").style.backgroundColor = 'rgb(235, 125, 62)'
+
+let emergencyTasksTitle = document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = 'rgb(220, 61, 235)'
+
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = 'rgb(220, 61, 235)'
+
+let noEmergencyTasks = document.querySelector(".no-emergency-tasks").style.backgroundColor = 'rgb(240, 217, 11)'
+
+let noEmergencyTasksTitle = document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = 'black'
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = 'black'
